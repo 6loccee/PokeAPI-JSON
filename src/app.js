0a4cb68f201971cd19e5253bdaf3f4f9658e349d
6loccee <martiin17jr@gmail.com>
2024-07-07 22:36:58 +0200

    feat: add news middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const routes = require('./routes/index')
+const path = require('path')
+const morgan = require('morgan')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.json())
+app.use(morgan('dev'))
+
 app.use('/', routes)
 
 module.exports = app
